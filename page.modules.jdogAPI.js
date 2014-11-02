abf9e17abc4c4931397eda7ac94b08ece937a020
justinkempton <justin@kempton.com>
2014-11-01 19:49:48 -0700

    adding new files

---
+++ page.modules.jdogAPI.js
@@ -0,0 +1,46 @@
+PAGE.addWait(
+	"Modules.jdogAPI"
+
+	, [
+		"ajax"
+		, "Constructors.APIMethod" 
+		, "ready" // dom is loaded
+	]
+
+	, function(ref) {
+
+		var dog = {
+			data : {}
+			, methods : []
+			, e_root : document.querySelector("#API")
+		}
+		, J = jDog
+
+		function build() {
+			dog.methods.length = 0
+
+			for (var x in dog.data.Methods) {
+				dog.methods.push(
+					ref.APIMethod(dog.e_root, dog.data.Methods[x])
+				)
+			}
+
+		}
+
+		function init() {
+
+			J.ajax.get(
+				"/jdogAPI.json"
+				, {}
+				, function(raw) {
+					var data = dog.data = JSON.parse(raw)
+					build()
+			})
+
+		}
+
+		init()
+
+		return dog
+
+	})
